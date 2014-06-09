8aea4cc390f5db591e7e26f8a5d587ad09b25c15
yureegasven <edgemaster@sgfc.co>
2014-06-09 18:31:04 +1000

    gtk tiem boyz

--- Makefile
+++ Makefile
@@ -1,4 +1,14 @@
-EmojiMaker : main.o
-	gcc main.o -o EmojiMaker
-main.o : main.c
-	gcc -c main.c -std=c99 -g
+NAME=EmojiMaker
+CFLAGS=-g -std=c99 -Wall -o $(NAME)
+GTKFLAGS=`pkg-config --cflags --libs gtk+-3.0`
+SRCS=main.c
+CC=gcc
+
+all : main
+
+main : $(SRCS)
+	$(CC) $(CFLAGS) $(SRCS) $(GTKFLAGS)
+#EmojiMaker : main.o
+#	gcc `pkg-config --cflags gtk+-3.0` main.o -o EmojiMaker
+#main.o : main.c
+#	gcc -c `pkg-config --libs gtk+-3.0` main.c   -g
--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <memory.h>
 #include <ctype.h>
+#include <gtk/gtk.h>
 
 
 void getLetter(char let, int letter[5][5]){
@@ -129,11 +130,11 @@ void getLetter(char let, int letter[5][5]){
     }
     else if(inp == 'M'){
         int M[5][5] = {
-            { 0, 1, 0, 1, 0 },
+            { 1, 1, 1, 1, 1 },
             { 1, 0, 1, 0, 1 },
             { 1, 0, 1, 0, 1 },
             { 1, 0, 1, 0, 1 },
-            { 1, 0, 0, 0, 1 }
+            { 1, 0, 1, 0, 1 }
         };  
         memcpy(letter,M,sizeof(M));
     }
@@ -222,7 +223,7 @@ void getLetter(char let, int letter[5][5]){
             { 1, 0, 0, 1 },
             { 1, 0, 0, 1 },
             { 1, 0, 0, 1 },
-            { 0, 1, 1, 0 },
+            { 1, 0, 0, 1 },
             { 0, 1, 1, 0 }
         };  
         memcpy(letter,V,sizeof(V));
@@ -265,34 +266,50 @@ void getLetter(char let, int letter[5][5]){
             { 1, 0, 0, 0 },
             { 1, 1, 1, 1 }
         };  
-        memcpy(letter,Z,sizeof(letter));
+        memcpy(letter,Z,sizeof(Z));
+    }
+    else if(inp == '?'){
+        int QU[5][5] = {
+            { 1, 1, 1, 1 },
+            { 0, 0, 0, 1 },
+            { 0, 1, 1, 1 },
+            { 0, 0, 0, 0 },
+            { 0, 1, 0, 0 }
+        };  
+        memcpy(letter,QU,sizeof(QU));
+    }
+    else if(inp == '1'){
+        int One[5][5] = {
+            { 1, 1, 1, 0 },
+            { 0, 1, 1, 0 },
+            { 0, 1, 1, 0 },
+            { 0, 1, 1, 0 },
+            { 1, 1, 1, 1 }
+        };  
+        memcpy(letter,One,sizeof(One));
     }
 }
 
-int main()
+typedef struct {
+    GtkWidget *inputw;
+    GtkWidget *emoji1;
+    GtkWidget *emoji2;
+    GtkClipboard *clipb;
+}widgets;
+
+static void getEmoteText (GtkWidget *widget,
+                         widgets *mydata,
+                         gpointer   data)
 {
     int row, col;
-    char word[500];
-    char emote1[25];
-    char emote2[25];
-
-    printf("%s\n","Enter the word");
-    scanf("%s",word);
-
-    printf("%s\n","Enter first emoticon with colons. This will be used for the Letters.");
-    scanf("%s",emote1);
-
-    printf("%s\n","Enter first emoticon with colons. This will be used for the Blank Space.");
-    scanf("%s",emote2);
-    printf("\n");
-    
+    const char* word = gtk_entry_get_text(GTK_ENTRY(mydata->inputw));
+    const char* emote1 = gtk_entry_get_text(GTK_ENTRY(mydata->emoji1));
+    const char* emote2 = gtk_entry_get_text(GTK_ENTRY(mydata->emoji2)); 
+    gtk_clipboard_set_text(mydata->clipb,word,-1);
     int max; 
-    int length = (4*strlen(word)) + strlen(word)-1;
     int letter[5][5];
     memset(letter, 0, sizeof(letter[0][0]) * 5 * 5);
-    int fcol = 0;
     for(row = 0; row < 5; row++){
-        fcol = 0;
         for(int i = 0; i < strlen(word); i++){
             max = 4;
             if(toupper(word[i]) == 'W' || toupper(word[i]) == 'M')
@@ -311,6 +328,65 @@ int main()
         }
         printf("\n");
     }
+printf("\n");
 
+}
+
+
+
+int main(int argc, char *argv[])
+{
+    GtkWidget *window;
+    GtkWidget *button;
+    GtkWidget *inputw;
+    GtkWidget *emoji1;
+    GtkWidget *emoji2;
+    GtkWidget *grid;
+    GtkClipboard *clipb;
+    
+    widgets data;
+    
+    
+    gtk_init (&argc, &argv);
+    
+    grid = gtk_grid_new ();
+    
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    
+    inputw = gtk_entry_new ();
+    emoji1 = gtk_entry_new ();
+    emoji2 = gtk_entry_new ();
+    clipb = gtk_clipboard_get (GDK_SELECTION_CLIPBOARD);
+    
+    data.inputw = inputw;
+    data.emoji1 = emoji1;
+    data.emoji2 = emoji2;
+    data.clipb = clipb;
+    
+    
+    gtk_entry_set_placeholder_text(GTK_ENTRY(inputw), "Word to Convert");
+    gtk_entry_set_placeholder_text(GTK_ENTRY(emoji1), "Emote for Text");
+    gtk_entry_set_placeholder_text(GTK_ENTRY(emoji2), "Emote for Blank");
+
+    gtk_window_set_title(GTK_WINDOW(window), "EmojiMaker");
+    button = gtk_button_new_with_label("Convert Text");
+    
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit),NULL);
+    g_signal_connect(button,"clicked",G_CALLBACK(getEmoteText),&data);
+    
+    
+    gtk_container_set_border_width(GTK_CONTAINER(window), 40);
+    gtk_container_add(GTK_CONTAINER(window), grid);
+    
+    
+    gtk_grid_attach (GTK_GRID (grid), inputw, 0, 0, 2, 1);
+    gtk_grid_attach (GTK_GRID (grid), emoji1, 0, 1, 1, 1);
+    gtk_grid_attach (GTK_GRID (grid), emoji2, 1, 1, 1, 1);
+    gtk_grid_attach (GTK_GRID (grid), button, 0, 2, 2, 1);
+
+
+    
+    gtk_widget_show_all(window);
+    gtk_main();
     return 0;
 }
